8cf9c433b50c236fbd7a81db06a5eb33ff4e15ba
HyunSeok Seong <tjdgustjr112@gmail.com>
2024-01-25 14:21:05 +0900

    chore: 프레임워크 기반

--- Srcs/SoulsLikeSimulator/SoulSimul/Stdafx.h
+++ Srcs/SoulsLikeSimulator/SoulSimul/Stdafx.h
@@ -8,8 +8,41 @@
 #define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
 // Windows 헤더 파일
 #include <windows.h>
+
+// DirectX 3D 헤더
+#include <string.h>
+#include <wrl.h>
+#include <shellapi.h>
+
+#include <d3d12.h>
+#include <dxgi1_4.h>
+
+#include <d3dcompiler.h>
+
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+
+#include <dxgidebug.h>
+
+
 // C 런타임 헤더 파일입니다.
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
+
+
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+#pragma comment(lib, "dxguid.lib")
+
+using namespace std;
+
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+using Microsoft::WRL::ComPtr;
