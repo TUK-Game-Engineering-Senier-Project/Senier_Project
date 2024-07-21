5baeea897f56ed86affe59352b341c72c8c319c1
Cho Seung Wan <csw4412@gmail.com>
2024-07-21 13:30:59 +0900

    Chore: 3D 강의자료 따라하기 6장까지 완료
    Chore: 3D 강의자료 따라하기 6장까지 완료

---
+++ Srcs/LabProject (6장까지 완료)/stdafx.h
@@ -0,0 +1,49 @@
+﻿// stdafx.h: 표준 시스템 포함 파일
+// 또는 프로젝트 특정 포함 파일이 들어 있는 포함 파일입니다.
+
+// 전체화면
+// #define _WITH_SWAPCHAIN_FULLSCREEN_STATE
+
+#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+// Windows 헤더 파일
+#include <windows.h>
+// C 런타임 헤더 파일입니다.
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+//  Direct3D와 관련있는 헤더 파일 추가
+
+#include <string>
+#include <wrl.h>
+#include <shellapi.h>
+
+#include <d3d12.h>
+#include <dxgi1_4.h>
+
+#include <D3Dcompiler.h>
+
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+
+#include <DXGIDebug.h>
+
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+using Microsoft::WRL::ComPtr;
+
+// import 라이브러리 추가
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "dxguid.lib")
+
+// 클라이언트 및 후면 버퍼 크기
+#define FRAME_BUFFER_WIDTH  800
+#define FRAME_BUFFER_HEIGHT 600
